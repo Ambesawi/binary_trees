55fd125add55c3bafd2b794f3c75336d7a1fae3a
Ambesawi <ambesawi77@gmail.com>
2023-05-30 13:29:33 -0700

    binary_trees.h

---
+++ binary_trees.h
@@ -0,0 +1,24 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* _BINARY_TREES_H_ */
